daf8aee8879cfa9481e29258bc638463e4c7d55d
lk.snjw <lk.snjw@gmail.com>
2025-04-28 18:30:45 +0530

    front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,23 @@ import {
 } from "react-router-dom";
 
 import MainLayout from "./Layouts/MainLayout";
+import DriverLayout from "./Layouts/DriverLayout";
+
 
 import HomePage from "./Pages/HomePage";
 import NotFound from "./Pages/NotFound";
 import Login from "./Pages/Customer/LoginForm";
 import Register from "./Pages/Customer/RegisterForm";
+import DriverList from "./Components/Admin/DriverList";
+import DriverLogin from "./Pages/Driver/LoginForm";
+import DriverProfile from "./Pages/Driver/DriverProfile";
+import DriverDashboard from "./Pages/Driver/DriverDashboard";
+import DriverDeliveries from "./Pages/Driver/DriverDeliveries";
+import OngoingDeliveries from "./Pages/Driver/OngoingDeliveries";
+import DriverIncome from "./Pages/Driver/DriverIncome";
+import DriverRegister from "./Pages/Driver/RegisterForm";
+import AllDeliveries from './Pages/Delivery/AllDeliveries';
+import DeliveryDetails from './Pages/Delivery/DeliveryDetails';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +35,27 @@ const router = createBrowserRouter(
 
         {/* Catch-all for 404 */}
         <Route path="*" element={<NotFound />} />
+		 {/* Driver section with its own layout */}
+		 
+		 <Route path="/admin/drivers" element={<DriverList />} />
+        <Route path="/register-driver" element={<DriverRegister />} />
+        <Route path="/deliveries" element={<AllDeliveries />} />
+        <Route path="/deliveries/:id" element={<DeliveryDetails />} />
+        
+		 
+		 
+      <Route path="/driver" element={<DriverLayout />}>
+        <Route path="login" element={<DriverLogin />} />
+        <Route path="dashboard" element={<DriverDashboard />} />
+        <Route path="profile" element={<DriverProfile />} />
+        <Route path="deliveries" element={<DriverDeliveries />} />
+        <Route path="ongoing" element={<OngoingDeliveries />} />
+        <Route path="income" element={<DriverIncome />} />
+        <Route path="deliveries/:id" element={<DeliveryDetails />} />
+        <Route path="*" element={<NotFound />} />
+      
+		
+		
       </Route>
     </>
   )
