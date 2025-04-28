de0550139a0a281b7d8ae5c61ebb27584f0e8b40
Gayashan <hansagayashan7@gmail.com>
2025-04-28 14:31:41 +0530

    modified payment and order

--- order-service/src/routes/product.route.js
+++ order-service/src/routes/product.route.js
@@ -1,12 +1,13 @@
 // src/route/product.route.js
 
 import express from 'express';
-import { createProduct, getAllProducts, getProductsById } from '../controller/product.controller.js';
+import { createProduct, getAllProducts, getProductsById, getProductsByRestaurant } from '../controller/product.controller.js';
 
 const router = express.Router();
 
 router.post('/', createProduct); 
 router.get('/:id', getProductsById); 
 router.get('/', getAllProducts); 
+router.get('/restaurant/:restaurantId', getProductsByRestaurant); // New endpoint for restaurant products
 
 export default router;
--- payment-service/src/routes/payment.route.js
+++ payment-service/src/routes/payment.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createPaymentIntent } from '../controller/payment.controllers.js';
+import { createPaymentIntent, confirmPayment } from '../controller/payment.controllers.js';
 
 const router = express.Router();
 
 router.post('/create-payment-intent', createPaymentIntent);
+router.post('/confirm-payment', confirmPayment);
 
 export default router;
